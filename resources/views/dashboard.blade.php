644ac3468422949a2bafd7869aea88c30b307842
Jordy de Ron <jordyderon@outlook.com>
2021-12-08 14:02:17 +0100

    dashboard belangrijke info (niet af atm)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,20 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="matches">
+
+                        @foreach ($games as $game)
+                            <div>
+                                <p>{{ $game->team1->name }} - {{ $game->team2->name }}</p>
+                                <p>{{ $game->datetime }}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="teams">
+                        @foreach ($teams as $team)
+                                <p>{{ $team->name }}</p>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
