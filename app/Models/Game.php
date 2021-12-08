0f1f0c98803f65e0aa670c2cd767acbb9ce6960f
MarijnvSprundel <71122319+MarijnvSprundel@users.noreply.github.com>
2021-12-08 14:25:08 +0100

    Added game model

---
+++ app/Models/Game.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use app\Models\User;
+use mysql_xdevapi\Table;
+
+class Game extends Model
+{
+    use HasFactory;
+    protected $table = 'games';
+    protected $fillable = [
+        'id',
+        'team1_id',
+        'team2_id',
+        'team1_score',
+        'team2_score',
+        'field_id',
+        'referee_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function team1(){
+        return Team::where('id', $this->team_id1)->get();
+    }
+    public function team2(){
+        return Team::where('id', $this->team_id2)->get();
+    }
+
+}
