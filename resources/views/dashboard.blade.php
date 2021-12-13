f2f5f8ff765be9c5c608108e2cba55d365043ec7
Faab007NL <mail@fabiandingemans.nl>
2021-12-13 09:30:58 +0100

    Merged other stuff

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-
                     <div class="matches">
                         @if (count($games) == 0)
                             <p>Er zijn geen wedstrijden</p>
@@ -17,7 +16,7 @@
                             @foreach ($games as $game)
                                 <div>
                                     <p>{{ $game->team1->name }} - {{ $game->team2->name }}</p>
-                                    {{-- <p>{{ $game->datetime }}</p> --}}
+                                    <p>{{ $game->datetime }}</p>
                                 </div>
                             @endforeach
                         @endif
